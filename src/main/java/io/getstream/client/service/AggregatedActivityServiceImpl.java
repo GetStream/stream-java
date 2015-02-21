3222e1dc5aa4a9735313a4048b0e16d170577e56
sirio7g <x25soc71>
2015-02-21 17:42:44 +0100

    Refactoring in order to return StreamResponse

---
+++ src/main/java/io/getstream/client/service/AggregatedActivityServiceImpl.java
@@ -0,0 +1,33 @@
+package io.getstream.client.service;
+
+import io.getstream.client.exception.StreamClientException;
+import io.getstream.client.model.activities.AggregatedActivity;
+import io.getstream.client.model.activities.BaseActivity;
+import io.getstream.client.model.beans.StreamResponse;
+import io.getstream.client.model.feeds.BaseFeed;
+import io.getstream.client.model.filters.FeedFilter;
+import io.getstream.client.repo.StreamRepository;
+
+import java.io.IOException;
+
+/**
+ * Provide methods to interact with Aggregated activities of subtype of {@link BaseActivity}.
+ * @param <T>
+ */
+public class AggregatedActivityServiceImpl<T extends BaseActivity> extends AbstractActivityService<T>
+        implements AggregatedActivityService<T> {
+
+    public AggregatedActivityServiceImpl(BaseFeed feed, Class<T> type, StreamRepository streamRepository) {
+		super(feed, type, streamRepository);
+    }
+
+    @Override
+    public StreamResponse<AggregatedActivity<T>> getActivities() throws IOException, StreamClientException {
+        return streamRepository.getAggregatedActivities(this.feed, this.type, new FeedFilter.Builder().build());
+    }
+
+    @Override
+	public StreamResponse<AggregatedActivity<T>> getActivities(final FeedFilter filter) throws IOException, StreamClientException {
+		return streamRepository.getAggregatedActivities(this.feed, this.type, filter);
+	}
+}
---
+++ src/main/java/io/getstream/client/service/NotificationActivityServiceImpl.java
@@ -0,0 +1,39 @@
+package io.getstream.client.service;
+
+import io.getstream.client.exception.StreamClientException;
+import io.getstream.client.model.activities.BaseActivity;
+import io.getstream.client.model.activities.NotificationActivity;
+import io.getstream.client.model.beans.MarkedActivity;
+import io.getstream.client.model.beans.StreamResponse;
+import io.getstream.client.model.feeds.BaseFeed;
+import io.getstream.client.model.filters.FeedFilter;
+import io.getstream.client.repo.StreamRepository;
+
+import java.io.IOException;
+
+/**
+ * Provide methods to interact with Notification activities of subtype of {@link BaseActivity}.
+ * @param <T>
+ */
+public class NotificationActivityServiceImpl<T extends BaseActivity> extends AbstractActivityService<T>
+    implements NotificationActivityService<T> {
+
+    public NotificationActivityServiceImpl(BaseFeed feed, Class type, StreamRepository streamRepository) {
+        super(feed, type, streamRepository);
+    }
+
+    @Override
+	public StreamResponse<NotificationActivity<T>> getActivities() throws IOException, StreamClientException {
+		return streamRepository.getNotificationActivities(this.feed, this.type, new FeedFilter.Builder().build());
+	}
+
+    @Override
+    public StreamResponse<NotificationActivity<T>> getActivities(final FeedFilter filter, final boolean markAsRead, final boolean markAsSeen) throws IOException, StreamClientException {
+		return streamRepository.getNotificationActivities(this.feed, this.type, filter, markAsRead, markAsSeen);
+	}
+
+    @Override
+	public StreamResponse<NotificationActivity<T>> getActivities(final FeedFilter filter, MarkedActivity markAsRead, MarkedActivity markAsSeen) throws IOException, StreamClientException {
+		return streamRepository.getNotificationActivities(this.feed, this.type, new FeedFilter.Builder().build(), markAsRead, markAsSeen);
+	}
+}
