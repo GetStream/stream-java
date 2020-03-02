GetStream/stream-java

commit 3b3da4a494b75ddd60814e2620830011780912da

Add pagination support for reaction (#61)
Added pagination support for reaction
getstream.io/docs_rest/#reaction_pagination

src/main/java/io/getstream/client/ReactionsClient.java
@@ -6,6 +6,7 @@ import io.getstream.core.StreamReactions;
 import io.getstream.core.exceptions.StreamException;
 import io.getstream.core.http.Token;
 import io.getstream.core.models.FeedID;
+import io.getstream.core.models.Pagenated;
 import io.getstream.core.models.Reaction;
 import io.getstream.core.options.Filter;
 import io.getstream.core.options.Limit;
@@ -60,6 +61,40 @@ public final class ReactionsClient {
     public CompletableFuture<List<Reaction>> filter(LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
         final Token token = buildReactionsToken(secret, TokenAction.READ);
         return reactions.filter(token, lookup, id, filter, limit, kind);
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id) throws StreamException {
+        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, DefaultOptions.DEFAULT_LIMIT, "");
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Limit limit) throws StreamException {
+        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, limit, "");
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Filter filter) throws StreamException {
+        return pagenatedFilter(lookup, id, filter, DefaultOptions.DEFAULT_LIMIT, "");
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, String kind) throws StreamException {
+        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, DefaultOptions.DEFAULT_LIMIT, kind);
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Filter filter, Limit limit) throws StreamException {
+        return pagenatedFilter(lookup, id, filter, limit, "");
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Limit limit, String kind) throws StreamException {
+        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, limit, kind);
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
+        final Token token = buildReactionsToken(secret, TokenAction.READ);
+        return reactions.pagenatedFilter(token, lookup, id, filter, limit, kind);
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(String next) throws StreamException {
+        final Token token = buildReactionsToken(secret, TokenAction.READ);
+        return reactions.pagenatedFilter(token, next);
     }
 
     public CompletableFuture<Reaction> add(String userID, String kind, String activityID, Iterable<FeedID> targetFeeds) throws StreamException {

src/main/java/io/getstream/core/StreamReactions.java
@@ -1,11 +1,13 @@
 package io.getstream.core;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.collect.ImmutableMap;
 import io.getstream.core.exceptions.StreamException;
 import io.getstream.core.http.HTTPClient;
 import io.getstream.core.http.Token;
 import io.getstream.core.models.FeedID;
+import io.getstream.core.models.Pagenated;
 import io.getstream.core.models.Reaction;
 import io.getstream.core.options.CustomQueryParameter;
 import io.getstream.core.options.Filter;
@@ -59,6 +61,25 @@ public final class StreamReactions {
         }
     }
 
+    public CompletableFuture<Pagenated> getPagenated(Token token, String id) throws StreamException {
+        checkNotNull(id, "Reaction id can't be null");
+        checkArgument(!id.isEmpty(), "Reaction id can't be empty");
+
+        try {
+            final URL url = buildReactionsURL(baseURL, id + '/');
+            return httpClient.execute(buildGet(url, key, token))
+                .thenApply(response -> {
+                    try {
+                        return deserialize(response, Pagenated.class);
+                    } catch (StreamException | IOException e) {
+                        throw new CompletionException(e);
+                    }
+                });
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new StreamException(e);
+        }
+    }
+
     public CompletableFuture<List<Reaction>> filter(Token token, LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
         checkNotNull(lookup, "Lookup kind can't be null");
         checkNotNull(id, "Reaction ID can't be null");
@@ -77,6 +98,47 @@ public final class StreamReactions {
                             throw new CompletionException(e);
                         }
                     });
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new StreamException(e);
+        }
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(Token token, LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
+        checkNotNull(lookup, "Lookup kind can't be null");
+        checkNotNull(id, "Reaction ID can't be null");
+        checkArgument(!id.isEmpty(), "Reaction ID can't be empty");
+        checkNotNull(filter, "Filter can't be null");
+        checkNotNull(kind, "Kind can't be null");
+
+        try {
+            final URL url = buildReactionsURL(baseURL, lookup.getKind() + '/' + id + '/' + kind);
+            RequestOption withActivityData = new CustomQueryParameter("with_activity_data", Boolean.toString(lookup == LookupKind.ACTIVITY_WITH_DATA));
+            return httpClient.execute(buildGet(url, key, token, filter, limit, withActivityData))
+                .thenApply(response -> {
+                    try {
+                        return deserialize(response, new TypeReference<Pagenated<Reaction>>() {});
+                    } catch (StreamException | IOException e) {
+                        throw new CompletionException(e);
+                    }
+                });
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new StreamException(e);
+        }
+    }
+
+    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(Token token, String next) throws StreamException {
+        checkNotNull(next, "next can't be null");
+
+        try {
+            final URL url = new URL(baseURL, next);
+            return httpClient.execute(buildGet(url, key, token))
+                .thenApply(response -> {
+                    try {
+                        return deserialize(response, new TypeReference<Pagenated<Reaction>>() {});
+                    } catch (StreamException | IOException e) {
+                        throw new CompletionException(e);
+                    }
+                });
         } catch (MalformedURLException | URISyntaxException e) {
             throw new StreamException(e);
         }

src/test/java/io/getstream/client/ReactionsClientTest.java
@@ -3,6 +3,7 @@ package io.getstream.client;
 import io.getstream.core.LookupKind;
 import io.getstream.core.models.Activity;
 import io.getstream.core.models.FeedID;
+import io.getstream.core.models.Pagenated;
 import io.getstream.core.models.Reaction;
 import org.junit.Test;
 
@@ -39,6 +40,27 @@ public class ReactionsClientTest {
         client.reactions().filter(LookupKind.ACTIVITY, activity.getID()).join();
 
         List<Reaction> result = client.reactions().filter(LookupKind.ACTIVITY_WITH_DATA, activity.getID(), "comment").join();
+    }
+
+    @Test
+    public void pagedFilter() throws Exception {
+        Client client = Client.builder(apiKey, secret).build();
+
+        Activity activity = client.flatFeed("flat", "reactor").addActivity(Activity.builder()
+            .actor("this")
+            .verb("done")
+            .object("that")
+            .build()).join();
+
+        client.reactions().add("john-doe", "like", activity.getID()).join();
+
+        client.reactions().pagenatedFilter(LookupKind.ACTIVITY, activity.getID()).join();
+
+        Pagenated<Reaction> result = client.reactions()
+            .pagenatedFilter(LookupKind.ACTIVITY_WITH_DATA, activity.getID(), "comment").join();
+        while (result.getNext() != null && !result.getNext().isEmpty()) {
+            result = client.reactions().pagenatedFilter(result.getNext()).join();
+        }
     }
 
     @Test

src/main/java/io/getstream/core/models/Pagenated.java
@@ -1,0 +1,74 @@
+package io.getstream.core.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+import java.util.List;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Pagenated<T> {
+
+  private final String next;
+  private List<T> results;
+  private final String duration;
+
+  public String getNext() {
+    return next;
+  }
+
+  public List<T> getResults() {
+    return results;
+  }
+
+  public void setResults(List<T> results) {
+    this.results = results;
+  }
+
+  public String getDuration() {
+    return duration;
+  }
+
+  @JsonCreator
+  private Pagenated(
+      @JsonProperty("next")
+          String next,
+      @JsonProperty("results")
+          List<T> results,
+      @JsonProperty("duration")
+          String duration
+  ) {
+    this.next = next;
+    this.results = results;
+    this.duration = duration;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Pagenated that = (Pagenated) o;
+    return next.equals(that.next) &&
+        duration.equals(that.duration);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), next, duration);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("next", this.next)
+        .add("results", this.results)
+        .add("duration", this.duration)
+        .toString();
+  }
+
+}

commit 806dfe08758237bf45aea915ba7e6448bd35b30d

Rename pagenated to paginated (#63)
Fix the typo introduced in #61

src/main/java/io/getstream/client/ReactionsClient.java
@@ -6,7 +6,7 @@ import io.getstream.core.StreamReactions;
 import io.getstream.core.exceptions.StreamException;
 import io.getstream.core.http.Token;
 import io.getstream.core.models.FeedID;
-import io.getstream.core.models.Pagenated;
+import io.getstream.core.models.Paginated;
 import io.getstream.core.models.Reaction;
 import io.getstream.core.options.Filter;
 import io.getstream.core.options.Limit;
@@ -63,38 +63,38 @@ public final class ReactionsClient {
         return reactions.filter(token, lookup, id, filter, limit, kind);
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id) throws StreamException {
-        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, DefaultOptions.DEFAULT_LIMIT, "");
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(LookupKind lookup, String id) throws StreamException {
+        return paginatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, DefaultOptions.DEFAULT_LIMIT, "");
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Limit limit) throws StreamException {
-        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, limit, "");
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(LookupKind lookup, String id, Limit limit) throws StreamException {
+        return paginatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, limit, "");
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Filter filter) throws StreamException {
-        return pagenatedFilter(lookup, id, filter, DefaultOptions.DEFAULT_LIMIT, "");
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(LookupKind lookup, String id, Filter filter) throws StreamException {
+        return paginatedFilter(lookup, id, filter, DefaultOptions.DEFAULT_LIMIT, "");
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, String kind) throws StreamException {
-        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, DefaultOptions.DEFAULT_LIMIT, kind);
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(LookupKind lookup, String id, String kind) throws StreamException {
+        return paginatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, DefaultOptions.DEFAULT_LIMIT, kind);
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Filter filter, Limit limit) throws StreamException {
-        return pagenatedFilter(lookup, id, filter, limit, "");
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(LookupKind lookup, String id, Filter filter, Limit limit) throws StreamException {
+        return paginatedFilter(lookup, id, filter, limit, "");
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Limit limit, String kind) throws StreamException {
-        return pagenatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, limit, kind);
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(LookupKind lookup, String id, Limit limit, String kind) throws StreamException {
+        return paginatedFilter(lookup, id, DefaultOptions.DEFAULT_FILTER, limit, kind);
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
         final Token token = buildReactionsToken(secret, TokenAction.READ);
-        return reactions.pagenatedFilter(token, lookup, id, filter, limit, kind);
+        return reactions.paginatedFilter(token, lookup, id, filter, limit, kind);
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(String next) throws StreamException {
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(String next) throws StreamException {
         final Token token = buildReactionsToken(secret, TokenAction.READ);
-        return reactions.pagenatedFilter(token, next);
+        return reactions.paginatedFilter(token, next);
     }
 
     public CompletableFuture<Reaction> add(String userID, String kind, String activityID, Iterable<FeedID> targetFeeds) throws StreamException {

src/main/java/io/getstream/core/StreamReactions.java
@@ -7,22 +7,22 @@ import io.getstream.core.exceptions.StreamException;
 import io.getstream.core.http.HTTPClient;
 import io.getstream.core.http.Token;
 import io.getstream.core.models.FeedID;
-import io.getstream.core.models.Pagenated;
+import io.getstream.core.models.Paginated;
 import io.getstream.core.models.Reaction;
 import io.getstream.core.options.CustomQueryParameter;
 import io.getstream.core.options.Filter;
 import io.getstream.core.options.Limit;
 import io.getstream.core.options.RequestOption;
-import java8.util.J8Arrays;
-import java8.util.concurrent.CompletableFuture;
-import java8.util.concurrent.CompletionException;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import java8.util.J8Arrays;
+import java8.util.concurrent.CompletableFuture;
+import java8.util.concurrent.CompletionException;
+
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.base.Preconditions.checkArgument;
@@ -61,7 +61,7 @@ public final class StreamReactions {
         }
     }
 
-    public CompletableFuture<Pagenated> getPagenated(Token token, String id) throws StreamException {
+    public CompletableFuture<Paginated> getPaginated(Token token, String id) throws StreamException {
         checkNotNull(id, "Reaction id can't be null");
         checkArgument(!id.isEmpty(), "Reaction id can't be empty");
 
@@ -70,7 +70,7 @@ public final class StreamReactions {
             return httpClient.execute(buildGet(url, key, token))
                 .thenApply(response -> {
                     try {
-                        return deserialize(response, Pagenated.class);
+                        return deserialize(response, Paginated.class);
                     } catch (StreamException | IOException e) {
                         throw new CompletionException(e);
                     }
@@ -103,7 +103,7 @@ public final class StreamReactions {
         }
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(Token token, LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(Token token, LookupKind lookup, String id, Filter filter, Limit limit, String kind) throws StreamException {
         checkNotNull(lookup, "Lookup kind can't be null");
         checkNotNull(id, "Reaction ID can't be null");
         checkArgument(!id.isEmpty(), "Reaction ID can't be empty");
@@ -116,7 +116,7 @@ public final class StreamReactions {
             return httpClient.execute(buildGet(url, key, token, filter, limit, withActivityData))
                 .thenApply(response -> {
                     try {
-                        return deserialize(response, new TypeReference<Pagenated<Reaction>>() {});
+                        return deserialize(response, new TypeReference<Paginated<Reaction>>() {});
                     } catch (StreamException | IOException e) {
                         throw new CompletionException(e);
                     }
@@ -126,7 +126,7 @@ public final class StreamReactions {
         }
     }
 
-    public CompletableFuture<Pagenated<Reaction>> pagenatedFilter(Token token, String next) throws StreamException {
+    public CompletableFuture<Paginated<Reaction>> paginatedFilter(Token token, String next) throws StreamException {
         checkNotNull(next, "next can't be null");
 
         try {
@@ -134,7 +134,7 @@ public final class StreamReactions {
             return httpClient.execute(buildGet(url, key, token))
                 .thenApply(response -> {
                     try {
-                        return deserialize(response, new TypeReference<Pagenated<Reaction>>() {});
+                        return deserialize(response, new TypeReference<Paginated<Reaction>>() {});
                     } catch (StreamException | IOException e) {
                         throw new CompletionException(e);
                     }

src/main/java/io/getstream/core/models/Paginated.java
@@ -1,14 +1,14 @@
 package io.getstream.core.models;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.common.base.MoreObjects;
 import java.util.List;
 import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Pagenated<T> {
+public class Paginated<T> {
 
   private final String next;
   private List<T> results;
@@ -31,7 +31,7 @@ public class Pagenated<T> {
   }
 
   @JsonCreator
-  private Pagenated(
+  private Paginated(
       @JsonProperty("next")
           String next,
       @JsonProperty("results")
@@ -52,7 +52,7 @@ public class Pagenated<T> {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    Pagenated that = (Pagenated) o;
+    Paginated that = (Paginated) o;
     return next.equals(that.next) &&
         duration.equals(that.duration);
   }
@@ -70,5 +70,4 @@ public class Pagenated<T> {
         .add("duration", this.duration)
         .toString();
   }
-
 }

src/test/java/io/getstream/client/ReactionsClientTest.java
@@ -3,11 +3,11 @@ package io.getstream.client;
 import io.getstream.core.LookupKind;
 import io.getstream.core.models.Activity;
 import io.getstream.core.models.FeedID;
-import io.getstream.core.models.Pagenated;
+import io.getstream.core.models.Paginated;
 import io.getstream.core.models.Reaction;
+import java.util.List;
 import org.junit.Test;
 
-import java.util.List;
 
 public class ReactionsClientTest {
     private static final String apiKey = "gp6e8sxxzud6";
@@ -54,12 +54,12 @@ public class ReactionsClientTest {
 
         client.reactions().add("john-doe", "like", activity.getID()).join();
 
-        client.reactions().pagenatedFilter(LookupKind.ACTIVITY, activity.getID()).join();
+        client.reactions().paginatedFilter(LookupKind.ACTIVITY, activity.getID()).join();
 
-        Pagenated<Reaction> result = client.reactions()
-            .pagenatedFilter(LookupKind.ACTIVITY_WITH_DATA, activity.getID(), "comment").join();
+        Paginated<Reaction> result = client.reactions()
+            .paginatedFilter(LookupKind.ACTIVITY_WITH_DATA, activity.getID(), "comment").join();
         while (result.getNext() != null && !result.getNext().isEmpty()) {
-            result = client.reactions().pagenatedFilter(result.getNext()).join();
+            result = client.reactions().paginatedFilter(result.getNext()).join();
         }
     }
 