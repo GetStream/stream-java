7275ea80efa49a4428213515267cd38bd692ec0e
ibrahim-camelan <ibrahimcamelan@gmail.com>
2023-12-31 15:44:02 +0200

    [CM-6211] minor refactoring

--- src/main/java/io/getstream/core/faye/client/FayeErrorListener.java
+++ src/main/java/io/getstream/core/faye/client/FayeErrorListener.java
@@ -3,5 +3,5 @@ package io.getstream.core.faye.client;
 import okhttp3.Response;
 
 public interface FayeErrorListener {
-    void onError(Throwable t, Response response);
+    void onError(Throwable throwable, Response response);
 }
