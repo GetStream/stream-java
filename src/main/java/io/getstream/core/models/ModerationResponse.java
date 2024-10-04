fe94dcfc68cc70dc8bf4c306ea6fab2b4d5e89e3
Aditya Agarwal <itsmeadityaagarwal@gmail.com>
2024-10-04 11:20:11 +0200

    chore: add moderation template to activity

---
+++ src/main/java/io/getstream/core/models/ModerationResponse.java
@@ -0,0 +1,69 @@
+package io.getstream.core.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ModerationResponse {
+    private String Status;
+    private String RecommendedAction;
+    private APIError APIError;
+    private String OriginFeed;
+
+    // Default constructor
+    public ModerationResponse() {}
+
+    // Constructor with parameters
+    @JsonCreator
+    public ModerationResponse(
+        @JsonProperty("status") String status,
+        @JsonProperty("recommended_action") String recommendedAction,
+        @JsonProperty("api_error") APIError apiError,
+        @JsonProperty("origin_feed") String originFeed) {
+        this.Status = status;
+        this.RecommendedAction = recommendedAction;
+        this.APIError = apiError;
+        this.OriginFeed = originFeed;
+    }
+
+    // Getters
+    public String getStatus() {
+        return Status;
+    }
+
+    public String getRecommendedAction() {
+        return RecommendedAction;
+    }
+
+    public APIError getAPIError() {
+        return APIError;
+    }
+
+    public String getOriginFeed() {
+        return OriginFeed;
+    }
+}
+class APIError {
+    private String Code;
+    private String Message;
+
+    // Default constructor
+    public APIError() {}
+
+    // Constructor with parameters
+    @JsonCreator
+    public APIError(
+        @JsonProperty("code") String code,
+        @JsonProperty("message") String message) {
+        this.Code = code;
+        this.Message = message;
+    }
+
+    // Getters
+    public String getCode() {
+        return Code;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+}
