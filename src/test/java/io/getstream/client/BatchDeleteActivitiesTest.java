25a178d46cbe73bce6228119d155883e0bd302d4
Aditya Agarwal <itsmeadityaagarwal@gmail.com>
2024-11-07 17:58:50 +0100

    chore: add support for discard_deleted_activities

--- src/test/java/io/getstream/client/BatchDeleteActivitiesTest.java
+++ src/test/java/io/getstream/client/BatchDeleteActivitiesTest.java
@@ -2,17 +2,22 @@ package io.getstream.client;
 
 import static org.junit.Assert.*;
 
+import com.google.common.collect.Lists;
 import io.getstream.core.BatchDeleteActivities;
 import io.getstream.core.http.HTTPClient;
 import io.getstream.core.models.BatchDeleteActivitiesRequest;
 import io.getstream.core.models.BatchDeleteActivitiesRequest.ActivityToDelete;
 import io.getstream.core.models.Activity;
+import io.getstream.core.models.FeedID;
+import io.getstream.core.options.Filter;
+import io.getstream.core.options.Limit;
 import java8.util.concurrent.CompletableFuture;
 import org.junit.*;
 
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class BatchDeleteActivitiesTest {
 
@@ -35,6 +40,8 @@ public class BatchDeleteActivitiesTest {
     @Test
     public void testDeleteActivities() throws Exception {
         BatchDeleteActivitiesClient batchDeleteClient = client.batchDeleteActivities();
+        String uuid1 = UUID.randomUUID().toString().replace("-", "");
+        FlatFeed feed = client.flatFeed("flat", uuid1);
 
         // Insert some activities
         Activity activity1 = Activity.builder()
@@ -42,19 +49,19 @@ public class BatchDeleteActivitiesTest {
                 .verb("post")
                 .object("object1")
                 .build();
-        Activity activity1Res = client.flatFeed("user", "user1").addActivity(activity1).join();
+        Activity activity1Res = feed.addActivity(activity1).join();
 
         Activity activity2 = Activity.builder()
                 .actor("user1")
                 .verb("like")
                 .object("object2")
                 .build();
-        Activity activity2Res = client.flatFeed("user", "user1").addActivity(activity2).join();
+        Activity activity2Res = feed.addActivity(activity2).join();
 
         // Create delete request
         List<ActivityToDelete> activities = Arrays.asList(
-                new ActivityToDelete(activity1Res.getID(), Arrays.asList("user1")),
-                new ActivityToDelete(activity2Res.getID(), Arrays.asList("user1"))
+                new ActivityToDelete(activity1Res.getID(), Arrays.asList("user:user1", "user:alice")),
+                new ActivityToDelete(activity2Res.getID(), Arrays.asList("user:user1"))
         );
         BatchClient clientBatch = Client.builder(apiKey, secret).build().batch();
 
@@ -79,5 +86,67 @@ public class BatchDeleteActivitiesTest {
 
         List<Activity> deletedActivity2 = clientBatch.getActivitiesByID(activity2Res.getID()).join();
         assertEquals(0, deletedActivity2.size());
+    }
+
+    @Test
+    public void testDeleteActivitis() throws Exception {
+        BatchDeleteActivitiesClient batchDeleteClient = client.batchDeleteActivities();
+
+        // Insert some activities
+        String uuid1 = UUID.randomUUID().toString().replace("-", "");
+        String uuid2 = UUID.randomUUID().toString().replace("-", "");
+
+        FlatFeed feed = client.flatFeed("flat", uuid1);
+        FlatFeed feedAlice = client.flatFeed("flat", uuid2);
+        Activity activity1 = Activity.builder()
+                .actor("user1")
+                .verb("post")
+                .to(Lists.newArrayList(feedAlice.getID()))
+                .object("object1")
+                .build();
+        Activity activity1Res = feed.addActivity(activity1).join();
+
+        Activity activity2 = Activity.builder()
+                .actor("user1")
+                .verb("like")
+                .to(Lists.newArrayList(feedAlice.getID()))
+                .object("object2")
+                .build();
+        Activity activity2Res = feed.addActivity(activity2).join();
+
+        // Verify activities are inserted
+        List<Activity> activities =feed.getActivities(
+                new Limit(69), new Filter()).join();
+        assertEquals(2, activities.size());
+
+        // Create delete request
+        List<ActivityToDelete> activitiesToDelete = Arrays.asList(
+                new ActivityToDelete(activity1Res.getID(), Arrays.asList(feed.getID().toString())),
+                new ActivityToDelete(activity2Res.getID(), Arrays.asList(feedAlice.getID().toString()))
+        );
+        BatchDeleteActivitiesRequest request = new BatchDeleteActivitiesRequest(activitiesToDelete);
+        CompletableFuture<Object> future = batchDeleteClient.deleteActivities(request);
+        future.join();
+
+
+        // Verify activities are deleted by fetching by ID
+        BatchClient clientBatch = Client.builder(apiKey, secret).build().batch();
+        List<Activity> deletedActivity1 = clientBatch.getActivitiesByID(activity1Res.getID()).join();
+        assertEquals(0, deletedActivity1.size());
+
+        // read feeds
+        // without discardDeletedActivities
+        activities =feed.getActivities(
+                new Limit(69), new Filter()).join();
+        assertEquals(2, activities.size());
+
+
+        activities =feed.getActivities(
+                new Limit(10), new Filter().discardDeletedActivities()).join();
+        assertEquals(0, activities.size());
+
+        activities =feedAlice.getActivities(
+                new Limit(10), new Filter().discardDeletedActivities()).join();
+        assertEquals(1, activities.size());
     }
 }
