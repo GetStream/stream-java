aae5ea5d2f902ab4fd31e8e9066a8c57436b024f
Aditya Agarwal <itsmeadityaagarwal@gmail.com>
2025-11-26 17:46:33 +0100

    chore: add activity copy limit to follow

--- src/main/java/io/getstream/core/models/FollowRelation.java
+++ src/main/java/io/getstream/core/models/FollowRelation.java
@@ -1,9 +1,11 @@
 package io.getstream.core.models;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
 import java.util.Objects;
@@ -12,15 +14,26 @@ import java.util.Objects;
 public final class FollowRelation {
   private final String source;
   private final String target;
+  private final Integer activityCopyLimit;
 
   @JsonCreator
   public FollowRelation(
-      @JsonProperty("feed_id") String source, @JsonProperty("target_id") String target) {
+      @JsonProperty("feed_id") String source,
+      @JsonProperty("target_id") String target,
+      @JsonProperty("activity_copy_limit") Integer activityCopyLimit) {
     checkNotNull(source, "FollowRelation 'source' field required");
     checkNotNull(target, "FollowRelation 'target' field required");
+    if (activityCopyLimit != null) {
+      checkArgument(activityCopyLimit >= 0, "Activity copy limit must be non negative");
+    }
 
     this.source = source;
     this.target = target;
+    this.activityCopyLimit = activityCopyLimit;
+  }
+
+  public FollowRelation(String source, String target) {
+    this(source, target, null);
   }
 
   public String getSource() {
@@ -31,17 +44,25 @@ public final class FollowRelation {
     return this.target;
   }
 
+  @JsonProperty("activity_copy_limit")
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  public Integer getActivityCopyLimit() {
+    return this.activityCopyLimit;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     FollowRelation that = (FollowRelation) o;
-    return Objects.equals(source, that.source) && Objects.equals(target, that.target);
+    return Objects.equals(source, that.source)
+        && Objects.equals(target, that.target)
+        && Objects.equals(activityCopyLimit, that.activityCopyLimit);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(source, target);
+    return Objects.hash(source, target, activityCopyLimit);
   }
 
   @Override
@@ -49,6 +70,7 @@ public final class FollowRelation {
     return MoreObjects.toStringHelper(this)
         .add("source", this.source)
         .add("target", this.target)
+        .add("activityCopyLimit", this.activityCopyLimit)
         .toString();
   }
 }
